c90868062a01ad6cc7e647cd4977c5f2794cc253
Alina <alinapiatakova1309@gmail.com>
2026-08-09 17:52:54 +0300

    fix

--- app/@modal/(.)notes/[id]/NotePreview.client.tsx
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -2,8 +2,8 @@
 
 import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'next/navigation';
-import { fetchNoteById } from '../../../../../lib/api';
-import Modal from '../../../../../components/Modal/Modal';
+import { fetchNoteById } from '../../../../lib/api';
+import Modal from '../../../../components/Modal/Modal';
 import css from './NotePreview.client.module.css';
 
 export default function NotePreviewClient() {
